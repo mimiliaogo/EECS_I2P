c9cd5fd242faa6819e8b3c26c43a84c01bd3211a
mimiliaogo <44743062+mimiliaogo@users.noreply.github.com>
2018-12-11 23:04:35 +0800

    define direction more readable

--- week 13/10845 - Light Reflection/main.c
+++ week 13/10845 - Light Reflection/main.c
@@ -1,56 +1,53 @@
 #include <stdio.h>
-int h, w;
-int pr, pc;
+#define UL 3
+#define UR 0
+#define DL 2
+#define DR 1
+int pr, pc, h, w;
 int isfirst;
 void light(int r, int c, int dir)
 {
-
-    if ((r==1||r==h||c==1||c==w)&&isfirst!=1) {
+    if ((r==1||r==h||c==1||c==w)&&isfirst==0) {
         pr = r;
         pc = c;
         return;
-    }
-    else {
+    }else {
         isfirst = 0;
-        if (dir==0) light(r-1, c+1, dir);
-        else if (dir==1) light(r+1, c+1, dir);
-        else if (dir==2) light(r+1, c-1, dir);
-        else if (dir==3) light(r-1, c-1, dir);
+        if (dir==UL) light(r-1, c-1, dir);
+        else if (dir==UR) light(r-1, c+1, dir);
+        else if (dir==DL) light(r+1, c-1, dir);
+        else light(r+1, c+1, dir);
     }
 
 }
 int main()
 {
-
     int k, i;
-    int orir, oric;
     int dir=0;
-    scanf("%d%d%d%d%d",&h, &w, &orir,&oric, &k);
-    isfirst = 1;
-    pr = orir;
-    pc = oric;
-//    light(orir, oric, dir);
-//    if ((pr==h||pr==1)&&(pc==w||pc==1)) printf("(%d,%d)", pr, pc);
+    scanf("%d%d", &h, &w);
+    scanf("%d%d", &pr, &pc);
+    scanf("%d", &k);
     for (i=2; i<=k; i++) {
-        light(pr, pc, dir);
-        if ((pr==h||pr==1)&&(pc==w||pc==1)) break;
         isfirst = 1;
-        if (dir==0) {
-            if (pr==1) dir = 1;
-            else dir = 3;
+        light(pr, pc, dir);
+        if (((pr==1)||(pr==h))&&((pc==1)||(pc==w))) break;
+        if (dir==UR) {
+            if (pr==1) dir = DR;
+            else dir = UL;
         }
-        else if (dir==1) {
-            if (pr==h) dir = 0;
-            else dir = 2;
+        else if (dir==UL) {
+            if (pr==1) dir = DL;
+            else dir = UR;
         }
-        else if (dir==2) {
-            if (pr==h) dir = 3;
-            else dir = 1;
+        else if (dir==DL) {
+            if (pr==h) dir = UL;
+            else dir = DR;
         }
-        else {
-            if (pr==1) dir = 2;
-            else dir = 0;
+        else  {
+            if (pr==h) dir = UR;
+            else dir = DL;
         }
+
     }
     printf("(%d,%d)", pr, pc);
 }
