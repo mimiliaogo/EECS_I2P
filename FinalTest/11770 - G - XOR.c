87a2dd13e6637cb6c31e723af5bb24bf87441ec8
mimiliaogo <44743062+mimiliaogo@users.noreply.github.com>
2019-01-08 23:52:40 +0800

    revise g

--- FinalTest/11770 - G - XOR.c
+++ FinalTest/11770 - G - XOR.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
-int num[100005];
-int ks[25];
+int num[100010];
+int k_digit[25];
 int main()
 {
-    int n, i, temp=0, j;
+    int n, i, j;
+    int tmp = 0;
     long long int k = 0L;
     long long int sum = 0L;
     scanf("%d", &n);
     for (i=0; i<n; i++) {
         scanf("%d", &num[i]);
     }
-    //run through all the digits and judge 1 or 0 is more
     for (i=0; i<20; i++) {
         for (j=0; j<n; j++) {
-            if (((1<<i)&num[j])!=0) temp += 1;
-            if (temp>(n/2)) ks[i] = 1;
-            else ks[i] = 0;
+            if (((1<<i)&num[j])!=0) tmp+=1;
         }
-        temp = 0;
+        if (tmp>(n/2)) k_digit[i] = 1;
+        else k_digit[i] = 0;
+        tmp = 0;
     }
 
     for (i=0; i<20; i++) {
-        k += ks[i]<<i;
+        k += k_digit[i]<<i;
     }
+
     for (i=0; i<n; i++) {
         sum+= k ^ num[i];
     }
+
     printf("%lld\n", sum);
+
 }
